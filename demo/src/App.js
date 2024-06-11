91083ef2551547477e662b9185aaebeb02c7f9c8
Pawanrajuuu <pawanraaz145@gmail.com>
2024-06-11 08:23:38 +0530

    this is first commit

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,18 +1,12 @@
-import {useState} from 'react'
-
-const App = ()=>{
-  const [count,setterCount] = useState(0)
-  const decrease = ()=> setterCount(count-1)
-  const reset = ()=>setterCount(0)
-  const increase = () => setterCount(count+1)
-  return(
-    <>
-      <h1>Counter</h1>
-      <h2> {count}</h2>
-      <button onClick={decrease}>Decrease</button>
-      <button onClick={reset}>Reset</button>
-      <button onClick={increase}>Increase</button>
-    </>
+import React from 'react'
+import Calculator from './Components/Calculator'
+const App = () => {
+  return (
+    <div>
+      <Calculator/>
+      
+    </div>
   )
 }
+
 export default App
---
+++ demo/src/Components/Calculator/index.js
@@ -0,0 +1,58 @@
+
+import {useState,useEffect} from 'react'
+
+const Calculator = () => {
+
+  const storedHeight = JSON.parse(localStorage.getItem("height"))
+  const storedweight = JSON.parse(localStorage.getItem("weight"))
+  
+  const [height,setHeight] = useState(storedHeight !== null ? storedHeight:170)
+  const [weight,setWeight] = useState(storedweight !== null ? storedweight:60)
+  
+
+  const BmiCalculator =(height,weight)=>{
+    const heightInMeters = height/100
+    const bmi = weight/heightInMeters**2
+    return bmi.toFixed(2)
+  }
+
+
+  useEffect(()=>{
+    document.title = `bmi ${BmiCalculator(height,weight)}`
+  })
+
+  useEffect(()=>{
+    localStorage.setItem("height", JSON.stringify(height))
+  })
+  useEffect(()=>{
+    localStorage.setItem("weight",JSON.stringify(weight))
+  })
+
+  const increaseOnHeight = ()=>setHeight(preValue => preValue+1)
+  const decreaseOnHeight = ()=>setHeight(preValue => preValue-1)
+  const increaseOnWeight = ()=>setWeight(preValue => preValue+1)
+  const decreaseOnWeight = ()=>setWeight(preValue => preValue-1)
+
+  return (
+    <div>
+      <h1>BMI Calculator</h1>
+      <div>
+        <h1>Height</h1>
+        <p>{height}</p>
+        <button onClick ={increaseOnHeight}>Increase</button>
+        <button onClick = {decreaseOnHeight}>Decrease</button>
+      </div>
+      <div>
+        <h1>Weight</h1>
+        <p>{weight}</p>
+        <button onClick ={increaseOnWeight}>Increase</button>
+        <button onClick = {decreaseOnWeight}>Decrease</button>
+      </div>
+      <div>
+        <h1>{BmiCalculator(height,weight)}</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Calculator
